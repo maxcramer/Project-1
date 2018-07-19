4649f35ba8bef799c1872a0a783d45720e489c04
Max Cramer <maximiliancramer@yahoo.com>
2018-07-19 15:30:50 +0100

    Final Upload Project 1

--- js/app.js
+++ js/app.js
@@ -1,26 +1,28 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+  //GLOBAL VARIABLES
   const playArea = document.querySelector('.playArea');
   const score = document.querySelector('#score');
+  const scoreFinal = document.querySelector('#scoreFinal');
   const restartButton = document.querySelector('#restart');
-  const bullCount = document.querySelector('#bullCount');
+  const bullCountDisplay = document.querySelector('#bullCount');
   const startBtn = document.querySelector('.startBtn');
   const timerDisplay = document.querySelector('#timerScreen');
-
-  let playerScore = 0; // tried changing this to 0
-  let playerBullets = 3;
+  const playing = document.querySelector('.playArea');
+  const grandking = document.querySelector('#grandking');
   const animation = ['runAcross', 'runAcross2', 'runAcross3', 'runAcross4', 'runAcross5', 'runAcross6', 'runAcross7', 'runAcross8', 'runAcross9'];
   const title = document.querySelector('#title');
+  const gameOverDisplay = document.querySelector('.gameOverDisplay');
+
+  // LOCAL VARIABLES
+  let backgroundTrackAudio;
+  let playerScore = 0; // tried changing this to 0
+  let playerBullets = 3;
   // Logic for timer
   let timeRemaining = 30;
   let isRunning = false;
-  let intervalId;
-
+  let timerIntervalId;
 
   // For character movement in relation to mouse
-  const playing = document.querySelector('.playArea');
-
-  const grandking = document.querySelector('#grandking');
 
   playing.addEventListener('mousemove', (event) => {
     grandking.style.left = event.clientX + 'px';
@@ -39,17 +41,21 @@ window.addEventListener('DOMContentLoaded', () => {
       document.getElementById('playArea').append(targetDiv);
     }
   }
+  // DISPLAYS GAME OVER DIV
+  function displayGameOver() {
+    gameOverDisplay.style.visibility = 'visible';
+  }
 
-  const level1Targets = [
-    {className: 'firstTarget', imageSrc: './Characters/billc.png'},
-    {className: 'secondTarget', imageSrc: './Characters/caytlinj.png'},
-    {className: 'thirdTarget', imageSrc: './Characters/hillary.png'},
-    {className: 'fourthTarget', imageSrc: './Characters/manBearPig.png'},
-    {className: 'fifthTarget', imageSrc: './Characters/mrslave.png'},
-    {className: 'sixthTarget', imageSrc: './Characters/trump.png'}
+  const charTargets = [
+    { className: 'firstTarget', imageSrc: './Characters/billc.png', audioSrc: './Sounds/bill.mp3' },
+    { className: 'secondTarget', imageSrc: './Characters/caytlinj.png', audioSrc: './Sounds/caytlinj.mp3' },
+    { className: 'thirdTarget', imageSrc: './Characters/hillary.png', audioSrc: './Sounds/hillary.mp3'},
+    { className: 'fourthTarget', imageSrc: './Characters/manBearPig.png', audioSrc: './Sounds/manbearpig.mp3' },
+    { className: 'fifthTarget', imageSrc: './Characters/mrslave.png', audioSrc: './Sounds/mrslave.mp3' },
+    { className: 'sixthTarget', imageSrc: './Characters/trump.png', audioSrc: './Sounds/trump.mp3' }
   ];
 
-  addTargetsToPlayArea(level1Targets);
+  addTargetsToPlayArea(charTargets);
   const targets = document.querySelectorAll('.targetDiv');
 
   playArea.addEventListener('click', handlePlayAreaClick);
@@ -66,6 +72,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   };
 
+  // GENERATES RANDOM SPEEDS AND PATHS THAT CHARACTERS FOLLOW
   targets.forEach(target => {
     target.addEventListener('click', handleTargetClick);
     target.style.animationName = `${generateRandomAnimation()}`;
@@ -75,15 +82,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // RESTART BUTTON
   restartButton.addEventListener('click', function() {
-    console.log('Restarting!');
+    console.log('this is isrunning at restart button ->', isRunning);
     playerBullets = 3;
     playerScore = 0;
     timeRemaining = 30;
-    bullCount.innerHTML = playerBullets;
+    bullCountDisplay.innerHTML = playerBullets;
     score.innerHTML = playerScore;
     timerDisplay.innerHTML = timeRemaining;
-    // TODO: Update the DOM to reflect timeRemaining!
-
+    isRunning = false;
+    backgroundTrackAudio.pause();
+    backgroundTrackAudio = null;
+    clearInterval(timerIntervalId); // Stop the timer
+    gameOverDisplay.style.visibility = 'hidden';
     startTimer();
   });
 
@@ -92,21 +102,46 @@ window.addEventListener('DOMContentLoaded', () => {
   function increaseScore() {
     playerScore ++;
     score.innerHTML = playerScore;
+    scoreFinal.innerHTML = playerScore;
     isRunning = true;
   }
 
   function displayBulletCount() {
-    bullCount.innerHTML = playerBullets;
+    bullCountDisplay.innerHTML = playerBullets;
   }
+
+  function getAudioFromTarget(element) {
+    const imgSrc = element.getAttribute('src');
+    const target = charTargets.filter(target => target.imageSrc === imgSrc)[0];
+    console.log(target.audioSrc);
+    if(target) {
+      return target.audioSrc;
+    } else {
+      return null;
+    }
+  }
+  //SOUND EFFECTS FOR CHARACTERS WHEN HIT
+  function playAudio(element) {
+    const audio = new Audio();
+    audio.src = getAudioFromTarget(element);
+    audio.volume = 1;
+    console.log(audio);
+    audio.play();
+  }
+
   // Player bullet count
   function handlePlayAreaClick(event) {
     console.log('Is running?', isRunning);
     if(isRunning) {
+      document.querySelector('.playArea').classList.add('shoot-cursor');
+      setTimeout(() => document.querySelector('.playArea').classList.remove('shoot-cursor'), 150)
       console.log('What is the target class?', event.target.classList);
       if (event.target.classList.contains('targetImage')) {
-        console.log('HIT!');
+        console.log('----->', event.target);
+        playAudio(event.target);
+        //add sound soundEffect
         increaseScore();
-        bullCount.innerHTML = playerBullets;
+        bullCountDisplay.innerHTML = playerBullets;
         playerBullets += 0;
       } else {
         console.log('MISS!');
@@ -114,31 +149,44 @@ window.addEventListener('DOMContentLoaded', () => {
         displayBulletCount();
         // change this to be minusing points
         if (playerBullets === 0) {
-          clearInterval(intervalId);
-          isRunning = false;
-          window.alert('Game Over - Please CLick Restart');
+          gameOver();
         }
       }
     }
   }
 
+  function gameOver() {
+    clearInterval(timerIntervalId);
+    isRunning = false;
+    timeRemaining = 30;
+    playerBullets = 3;
+    backgroundTrackAudio.src = '';
+    displayGameOver();
+  }
+
   function startTimer() {
+    console.log('this is isrunning at start timer ->', isRunning);
+    //PLAY AUDIO FOR BACKING TRACK
+    // TODO: SHOULDN'T PLAY TWICE
+    if (!backgroundTrackAudio) {
+      backgroundTrackAudio = new Audio('./Sounds/backingTrack.mp3');
+      backgroundTrackAudio.volume = 0.4;
+      backgroundTrackAudio.play();
+    }
     if (!isRunning) {
       isRunning = true;
       title.style.visibility = 'hidden';
       startBtn.style.display = 'none';
       restartButton.style.display = 'block';
-      intervalId = setInterval(() => {
+      timerIntervalId = setInterval(() => {
         timeRemaining -= 1;
         document.querySelector('#timerScreen').innerHTML = timeRemaining;
         if (timeRemaining === 0) {
-          clearInterval(intervalId);
-          isRunning = false;
-          return window.alert('Game Over');
+          gameOver();
         }
       }, 1000);
     } else {
-      clearInterval(intervalId);
+      clearInterval(timerIntervalId);
       isRunning = false;
     }
   }
